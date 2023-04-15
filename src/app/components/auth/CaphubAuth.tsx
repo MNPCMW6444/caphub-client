92a8d127b3279c0c8482b20e31a1968a534fb69b
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-15 22:45:56 +0300

    Update CaphubAuth.tsx

--- src/app/components/auth/CaphubAuth.tsx
+++ src/app/components/auth/CaphubAuth.tsx
@@ -1,3 +1,11 @@
-const CapHubAuth = () => <></>;
+import Box from "@mui/material/Box";
+
+const CapHubAuth = () => (
+  <>
+    <Box height="100vh" width="100wh" bgcolor="black">
+      asdasd
+    </Box>
+  </>
+);
 
 export default CapHubAuth;
