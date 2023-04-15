b9c8c127ec635fc4e0437e5548ea89644584d61a
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-15 20:42:48 +0300

    Update ProvideMainServer.tsx

--- src/app/providers/servers/ProvideMainServer.tsx
+++ src/app/providers/servers/ProvideMainServer.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import domain from "../../util/config/domain";
 
@@ -9,10 +9,12 @@ interface ProvideMainServerProps {
 
 const DEFAULT_TRY_INTERVAL = 500;
 
+const IDLE = "IDLE";
+const CHECKING_MESSAGE = "Checking server availability...";
 const BAD_MESSAGE = "Server is not available. Please try again later.";
 const GOOD_STATUS = "good";
 
-const checkIfServerIsThere = async () => {
+const checkServerAvailability = async () => {
   try {
     return (await axios.get(domain + "areyoualive")).data.answer === "yes"
       ? GOOD_STATUS
@@ -26,26 +28,28 @@ const ProvideMainServer = ({
   children,
   tryInterval,
 }: ProvideMainServerProps) => {
-  const [status, setStatus] = useState<string>(
-    "Checking server availability..."
-  );
+  const [status, setStatus] = useState<string>(IDLE);
+  const statusRef = useRef(status);
 
   useEffect(() => {
-    const setStatusAsyncly = async () =>
-      setStatus(await checkIfServerIsThere());
-    setStatusAsyncly();
-    let tryAgain: NodeJS.Timer | undefined = undefined;
-    setTimeout(() => {
-      if (status !== GOOD_STATUS) {
-        tryAgain = setInterval(
-          () => status !== GOOD_STATUS && setStatusAsyncly(),
-          tryInterval || DEFAULT_TRY_INTERVAL
-        );
+    statusRef.current = status;
+  }, [status]);
+
+  useEffect(() => {
+    const setStatusAsyncly = async () => {
+      setStatus(CHECKING_MESSAGE);
+      const newStatus = await checkServerAvailability();
+      setStatus(newStatus);
+
+      if (newStatus !== GOOD_STATUS) {
+        setTimeout(setStatusAsyncly, tryInterval || DEFAULT_TRY_INTERVAL);
       }
-    }, 2000);
-    status === GOOD_STATUS && tryAgain && clearInterval(tryAgain);
-    return () => tryAgain && clearInterval(tryAgain);
-  }, [status, tryInterval]);
+    };
+
+    if (statusRef.current === IDLE) {
+      setStatusAsyncly();
+    }
+  }, [tryInterval]);
 
   if (status === GOOD_STATUS) {
     return <>{children}</>;
