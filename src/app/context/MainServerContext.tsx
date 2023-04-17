4f504b983a3aecdce6f1bef0997ef5b95edfd9b7
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-17 11:38:45 +0300

    Update MainServerContext.tsx

--- src/app/context/MainServerContext.tsx
+++ src/app/context/MainServerContext.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import React from "react";
+import domain from "../util/config/domain";
 
 const axiosInstance = axios.create({
+  baseURL: `${domain}/`,
+  timeout: 4000,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
