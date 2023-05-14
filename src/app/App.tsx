5029861823ce1785adbdfb54b217efa8cea2d08f
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-05-14 21:04:36 +0300

    awd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import CaphubThemeProvider from "./providers/style/CaphubThemeProvider";
+import CaphubThemeProvider from "@caphub-group/theme-provider";
 import { UserContextProvider } from "./context/UserContext";
 import CaphubRouter from "./components/caphub-router/CaphubRouter";
 import { ProvideMainServer } from "@caphub-group/mainserver-provider";
